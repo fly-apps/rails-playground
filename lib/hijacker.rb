c201ab7f5c93c5956d750ed3ae470f99f2242ba1
Joshua Sierles <joshua@fly.io>
2022-08-26 18:14:16 +0200

    log in stdout

--- lib/hijacker.rb
+++ lib/hijacker.rb
@@ -15,8 +15,7 @@ class Hijacker
           begin
             right.write_nonblock(left.read_nonblock(4096))
           rescue => ex
-            # FIXME: env is not available here, logging is probably bad in this way
-            logger(env, :info, "Connection #{left} <-> #{right} closed due to #{ex}")
+            puts "Connection #{left} <-> #{right} closed due to #{ex}"
             cleanup(left, right)
           end
         end
@@ -36,41 +35,39 @@ class Hijacker
     port = 12345
 
     http = env['rack.hijack'].call
+    http ||= env["rack.hijack_io"]
+
+    puts "writing http response"
     # Write a proper HTTP response
-    http.write(http_response)
+    http.write_nonblock(http_response)
+
+    puts "writing http response"
+
     # Open the remote TCP socket
     sock = TCPSocket.new(host, port)
 
+    puts "Start proxying"
     # Start proxying
     @proxy.push(http, sock)
 
-    logger(env, :info, "Redirecting incoming request from #{env['REMOTE_ADDR']} to [#{host}]:#{port}")
+    puts "Redirecting incoming request from #{env['REMOTE_ADDR']} to [#{host}]:#{port}"
 
     # Rack requires this line below
-    return [200, {}, []]
-  rescue => ex
-    logger(env, :error, "#{ex.class} happened for #{env['REMOTE_ADDR']} trying to access #{host}:#{port}")
-    cleanup(http, sock)
-    return not_found # Return with a 404 error
+    # return [200, {}, []]
+  # rescue => ex
+  #   puts "#{ex.class} happened for #{env['REMOTE_ADDR']} trying to access #{host}:#{port}"
+  #   cleanup(http, sock)
+  #   return not_found # Return with a 404 error
   end
 
   private
 
-  def parse_headers(env)
-    case true
-    when %w(websocket).include?(env['HTTP_UPGRADE'])
-      logger(env, :info, "Upgrading to #{env['HTTP_UPGRADE']}")
-      return env['HTTP_UPGRADE'].to_sym
-    else
-      logger(env, :error, "Invalid upgrade request from #{env['REMOTE_ADDR']}")
-    end
-  end
-
   def http_response
     <<~HEREDOC.sub(/\n$/, "\n\n").gsub(/ {2,}/, '').gsub("\n", "\r\n")
     HTTP/1.1 101 Switching Protocols
-    Upgrade: websocket
     Connection: Upgrade
+    Sec-WebSocket-Protocol: actioncable-v1-json
+    Upgrade: websocket
     HEREDOC
   end
 
@@ -84,10 +81,5 @@ class Hijacker
     # Close the opened sockets and remove them from the proxy
     sockets.each { |sock| sock.close unless sock.closed? }
     @proxy.pop(*sockets) if sockets.length > 1
-  end
-
-  def logger(env, level, message)
-    # Do logging only if Rack::Logger is loaded as a middleware
-    env['rack.logger'].send(level, message) if env['rack.logger']
   end
 end
